c60e7b922a7a0df299e234110f5c2482e5301e48
Daniel Durango <daniel.durango@udea.edu.co>
2024-04-02 13:35:28 -0500

    El metodo Search esta completado

--- poo/src/main/java/com/pie/interfaces/NotesInterface.java
+++ poo/src/main/java/com/pie/interfaces/NotesInterface.java
@@ -11,6 +11,8 @@ public interface NotesInterface {
     String Read();
     List<Note> Search();
 
+    List<Note> SearchTitle(String title);
+
     List<Note> Add(Note note);
 
     Optional<Note> Write(Note note);
